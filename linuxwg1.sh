1045925c1ac383e6122f464fca52b2012fe01585
vivek magadum <66828419+vivek030@users.noreply.github.com>
2023-02-04 21:25:01 +0530

    Update linuxwg1.sh

--- linuxwg1.sh
+++ linuxwg1.sh
@@ -9,3 +9,4 @@ json='{"Email":"'"${email}"'","Identifier":"'"${id}"'"}'
 curl --silent  -f -k -X POST "https://newnew.korplink.com/api/v1/provisioning/peers" -H "accept: text/plain" -H "authorization: Basic $api" -H "Content-Type: application/json" -d $json -o "/etc/wireguard/wg0.conf"
 sudo systemctl start wg-quick@wg0
 sudo systemctl enable wg-quick@wg0
+resolvectl flush-caches
